fb8f931ab55b98273fc8e777b6159802d953056e
Donstesh <stephenshifoko@gmail.com>
2020-08-01 03:42:44 +0300

    redirect if authenticated user log in with correct

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -19,7 +19,15 @@ class RedirectIfAuthenticated
     public function handle($request, Closure $next, $guard = null)
     {
         if (Auth::guard($guard)->check()) {
-            return redirect(RouteServiceProvider::HOME);
+            
+            if($guard == "admin"){
+                //user was authenticated with admin guard.
+                return redirect()->route('admin.home');
+            } else {
+                //default guard.
+                return redirect()->route('home');
+            }
+
         }
 
         return $next($request);
