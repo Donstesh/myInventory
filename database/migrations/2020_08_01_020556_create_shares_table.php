bce7bdf560219c04c3b3d0aad090261f1e50e1e6
Donstesh <stephenshifoko@gmail.com>
2020-08-03 14:51:28 +0300

    shares

---
+++ database/migrations/2020_08_01_020556_create_shares_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSharesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('shares', function (Blueprint $table) {
+            $table->id();
+            $table->date('date_joined');
+            $table->string('detail');
+            $table->integer('amount_contributed');
+            $table->date('date_paid');
+            $table->integer('id_no');
+            $table->integer('phone_no');
+            $table->integer('next_of_kin');
+            $table->string('mode_of_payment');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('shares');
+    }
+}
