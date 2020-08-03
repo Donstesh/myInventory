fc8ca69e5a170cbe84c15c7c8b4b5a3ded1808c3
Donstesh <stephenshifoko@gmail.com>
2020-08-03 14:51:11 +0300

    Expenditure

---
+++ database/migrations/2020_08_01_020658_create_expenditures_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateExpendituresTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('expenditures', function (Blueprint $table) {
+            $table->id();
+            $table->date('date');
+            $table->string('detail');
+            $table->integer('quantity');
+            $table->string('category');
+            $table->integer('amount');
+            $table->string('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('expenditures');
+    }
+}
