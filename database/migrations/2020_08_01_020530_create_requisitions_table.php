848023200940895a4f0d7384b639d9a4a4fa7121
Donstesh <stephenshifoko@gmail.com>
2020-08-03 14:50:01 +0300

    requistions

---
+++ database/migrations/2020_08_01_020530_create_requisitions_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRequisitionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('requisitions', function (Blueprint $table) {
+            $table->id();
+            $table->date('date');
+            $table->string('detail');
+            $table->integer('quantity');
+            $table->string('requisition_amount');
+            $table->string('category');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('requisitions');
+    }
+}
