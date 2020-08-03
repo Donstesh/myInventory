93f2c9500f36b55f9f050b930044bcb309593069
Donstesh <stephenshifoko@gmail.com>
2020-08-03 15:48:53 +0300

    admin side bar

--- resources/views/inc/adminsidebar.blade.php
+++ resources/views/inc/adminsidebar.blade.php
@@ -43,17 +43,6 @@
                 <div class="navbar-default sidebar" role="navigation">
                     <div class="sidebar-nav navbar-collapse">
                         <ul class="nav" id="side-menu">
-                            <li class="sidebar-search">
-                                <div class="input-group custom-search-form">
-                                    <input type="text" class="form-control" placeholder="Search...">
-                                    <span class="input-group-btn">
-                                        <button class="btn btn-primary" type="button">
-                                            <i class="fa fa-search"></i>
-                                        </button>
-                                </span>
-                                </div>
-                                <!-- /input-group -->
-                            </li>
                             <li>
                                 <a href="{{ url('/admin/dashboard') }}" class="active"><i class="fa fa-dashboard fa-fw"></i> Dashboard</a>
                             </li>
