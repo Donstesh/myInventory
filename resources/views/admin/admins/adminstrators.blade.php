7c8e40289886c0d7e1d8f57ab1171ec4686f049d
Donstesh <stephenshifoko@gmail.com>
2020-09-07 01:52:02 +0300

    fixed Illuminate\Dbase\Eloquent\Collection::links.

--- resources/views/admin/admins/adminstrators.blade.php
+++ resources/views/admin/admins/adminstrators.blade.php
@@ -44,7 +44,6 @@
                         @endforeach
                         </tbody>
                     </table>
-                    {{ $admins->links() }}
             </div>
 
         </div>
