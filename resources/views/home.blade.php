38ab5b3c6a0b316199a068953a3183391948eea8
Donstesh <stephenshifoko@gmail.com>
2020-09-08 22:45:17 +0300

    Employee Home Dashboard

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,14 +1,90 @@
-@extends('layouts.app')
+@extends('layouts.user')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">User Dashboard</div>
-                <div class="card-body">You are logged in!</div>
+<br>
+<br>
+            <div id="page-wrapper">
+                <div class="container-fluid">
+                    <div class="row">
+                        <div class="col-lg-12">
+                            <h1 class="page-header">Employee HOME</h1>
+                        </div>
+                        <!-- /.col-lg-12 -->
+                    </div>
+                    <!-- /.row -->
+                    <div class="row">
+                        <div class="col-lg-3 col-md-6">
+                            <div class="panel panel-default">
+                                <div class="panel-heading">
+                                    <div class="row">
+                                        <div class="col-xs-3">
+                                        <i class="fas fa-clipboard fa-5x"></i>
+                                        </div>
+                                        <div class="col-xs-9 text-right">
+                                            <div class="huge">{{ $drpt }}</div>
+                                            <div>Daily Report</div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <a href="{{ url('admin/dailyreport/dailyreport') }}">
+                                    <div class="panel-footer">
+                                        <span class="pull-left">View Details</span>
+                                        <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+
+                                        <div class="clearfix"></div>
+                                    </div>
+                                </a>
+                            </div>
+                        </div>
+                        <div class="col-lg-3 col-md-6">
+                            <div class="panel panel-info">
+                                <div class="panel-heading">
+                                    <div class="row">
+                                        <div class="col-xs-3">
+                                        <i class="fas fa-prescription-bottle-alt fa-5x"></i>
+                                        </div>
+                                        <div class="col-xs-9 text-right">
+                                            <div class="huge">{{ $medics }}</div>
+                                            <div>Medication</div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <a href="{{ url('admin/medication/medication') }}">
+                                    <div class="panel-footer">
+                                        <span class="pull-left">View Details</span>
+                                        <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+
+                                        <div class="clearfix"></div>
+                                    </div>
+                                </a>
+                            </div>
+                        </div>
+                        <div class="col-lg-3 col-md-6">
+                            <div class="panel panel-green">
+                                <div class="panel-heading">
+                                    <div class="row">
+                                        <div class="col-xs-3">
+                                        <i class="fas fa-file-invoice fa-5x"></i>
+                                        </div>
+                                        <div class="col-xs-9 text-right">
+                                            <div class="huge">{{ $reqs }}</div>
+                                            <div>Requisition</div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <a href="{{ url('admin/req/requisition') }}">
+                                    <div class="panel-footer">
+                                        <span class="pull-left">View Details</span>
+                                        <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+
+                                        <div class="clearfix"></div>
+                                    </div>
+                                </a>
+                            </div>
+                        </div>
+                    <!-- /.row -->
+                </div>
+                <!-- /.container-fluid -->
             </div>
-        </div>
-    </div>
-</div>
-@endsection
+            <!-- /#page-wrapper -->
+  @endsection
