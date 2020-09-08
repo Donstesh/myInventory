ef90864295e1430706358a5cdd274194dd40f546
Donstesh <stephenshifoko@gmail.com>
2020-09-08 22:44:37 +0300

    Employee Dashboard Controller

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\User;
+use App\Admin;
+use App\Dailyeport;
+use App\Medication;
+use App\Shares;
+use App\Costoverhead;
+use App\Requisition;
+use DB;
 
 class HomeController extends Controller
 {
@@ -15,14 +23,33 @@ class HomeController extends Controller
     {
         $this->middleware('auth');
     }
-
     /**
-     * Show the application dashboard.
-     *
-     * @return \Illuminate\Contracts\Support\Renderable
+     * Show Admin Dashboard.
+     * 
+     * @return \Illuminate\Http\Response
      */
     public function index()
     {
-        return view('home');
+        $employees = User::count();
+        $admins = Admin::count();
+        $drpt = Dailyeport::count();
+        $medics = Medication::count();
+        $shares = Shares::count();
+        $cohs = Costoverhead::count();
+        $reqs = Requisition::count();
+        $totalreq = DB::table('requisitions')->where('status', 'Paid')->get()->sum('requisition_amount');
+        $totalcoh = DB::table('costoverheads')->where('status', 'Paid')->get()->sum('amount');
+        $totalexpenditure = ($totalcoh + $totalreq);
+        return view('home',['totalreq'=>$totalreq,
+                                                     'totalcoh'=>$totalcoh,
+                                                     'totalexpenditure'=>$totalexpenditure,
+                                                     'employees' => $employees,
+                                    'admins' => $admins,
+                                    'drpt' => $drpt,
+                                    'medics' => $medics,
+                                    'shares' => $shares,
+                                    'cohs' => $cohs,
+                                    'reqs' => $reqs,]);
     }
 }
+
