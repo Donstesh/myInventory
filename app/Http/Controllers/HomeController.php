f605c13bfd5405a8100c123e6aa3e832aca4cd69
Donstesh <stephenshifoko@gmail.com>
2020-09-09 23:10:23 +0300

    normal user home dashboard controller

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -30,25 +30,13 @@ class HomeController extends Controller
      */
     public function index()
     {
-        $employees = User::count();
-        $admins = Admin::count();
+        
         $drpt = Dailyeport::count();
         $medics = Medication::count();
-        $shares = Shares::count();
-        $cohs = Costoverhead::count();
         $reqs = Requisition::count();
-        $totalreq = DB::table('requisitions')->where('status', 'Paid')->get()->sum('requisition_amount');
-        $totalcoh = DB::table('costoverheads')->where('status', 'Paid')->get()->sum('amount');
-        $totalexpenditure = ($totalcoh + $totalreq);
-        return view('home',['totalreq'=>$totalreq,
-                                                     'totalcoh'=>$totalcoh,
-                                                     'totalexpenditure'=>$totalexpenditure,
-                                                     'employees' => $employees,
-                                    'admins' => $admins,
+        return view('home',[
                                     'drpt' => $drpt,
                                     'medics' => $medics,
-                                    'shares' => $shares,
-                                    'cohs' => $cohs,
                                     'reqs' => $reqs,]);
     }
 }
